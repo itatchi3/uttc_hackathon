f5e89b4012d3e4b0a190b31d9c72ae0f5176dec8
itaitoshiki <toshiki.i0918@gmail.com>
2022-08-01 22:30:08 +0900

    dbをgcp用に修正

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"os"
 	"uttc_backend/domain/model"
 
 	"github.com/jinzhu/gorm"
@@ -8,15 +10,15 @@ import (
 
 // NewDB DBと接続する
 func NewDB() *gorm.DB {
-	// mysqlUser := os.Getenv("MYSQL_USER")
-	// mysqlPwd := os.Getenv("MYSQL_PWD")
-	// mysqlHost := os.Getenv("MYSQL_HOST")
-	// mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	// connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	// db, err := gorm.Open("mysql", connStr)
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	db, err := gorm.Open("mysql", connStr)
 
-	db, err := gorm.Open("mysql", "user:password@tcp(hackathon_backend_db)/sample?charset=utf8mb4&parseTime=True&loc=Local")
+	// db, err := gorm.Open("mysql", "user:password@tcp(hackathon_backend_db)/sample?charset=utf8mb4&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
