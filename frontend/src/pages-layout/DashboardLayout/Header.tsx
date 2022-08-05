86e26c8dfd20f9f7ebe763d472645696023cf4df
itaitoshiki <toshiki.i0918@gmail.com>
2022-08-06 01:28:32 +0900

    いらないコード削除

--- frontend/src/pages-layout/DashboardLayout/Header.tsx
+++ frontend/src/pages-layout/DashboardLayout/Header.tsx
@@ -81,11 +81,7 @@ const UserMenu: FC = () => {
       transition="pop-top-right"
       control={
         <ActionIcon variant="hover" radius="xl" size={40}>
-          <Avatar
-            src={loginUser.profile_url}
-            // src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-            radius="xl"
-          />
+          <Avatar src={loginUser.profile_url} radius="xl" />
         </ActionIcon>
       }
       styles={(theme) => {
