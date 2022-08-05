79bbbd4a2333144467acab0fd1c820f7cbf95106
itaitoshiki <toshiki.i0918@gmail.com>
2022-08-05 10:48:35 +0900

    使ってないヘッダーコメントアウト

--- frontend/src/pages-layout/DashboardLayout/Header.tsx
+++ frontend/src/pages-layout/DashboardLayout/Header.tsx
@@ -1,20 +1,6 @@
-import {
-  ActionIcon,
-  Avatar,
-  Box,
-  Divider,
-  Group,
-  Indicator,
-  Menu,
-  Title,
-} from "@mantine/core";
-import { NextLink } from "@mantine/next";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { Box, Group, Title } from "@mantine/core";
 import type { FC, ReactNode } from "react";
 import { useGetChannelQuery } from "src/lib/channel/query";
-import { getPath } from "src/lib/const";
-import { Bell, Logout, Settings } from "tabler-icons-react";
 
 /** @package */
 export const Header: FC<{ left: ReactNode }> = ({ left }) => {
@@ -32,8 +18,8 @@ export const Header: FC<{ left: ReactNode }> = ({ left }) => {
       <Group spacing="lg" noWrap>
         {left}
         <ChannelName />
-        <Notification />
-        <UserMenu />
+        {/* <Notification /> */}
+        {/* <UserMenu /> */}
       </Group>
     </Box>
   );
@@ -66,59 +52,59 @@ const ChannelName: FC = () => {
   );
 };
 
-const Notification: FC = () => {
-  return (
-    <Indicator inline size={14} offset={4} color="red" withBorder>
-      <Link href={getPath("NOTIFICATION")} passHref>
-        <ActionIcon component="a" variant="hover" radius="xl" size={40}>
-          <Bell />
-        </ActionIcon>
-      </Link>
-    </Indicator>
-  );
-};
+// const Notification: FC = () => {
+//   return (
+//     <Indicator inline size={14} offset={4} color="red" withBorder>
+//       <Link href={getPath("NOTIFICATION")} passHref>
+//         <ActionIcon component="a" variant="hover" radius="xl" size={40}>
+//           <Bell />
+//         </ActionIcon>
+//       </Link>
+//     </Indicator>
+//   );
+// };
 
-const UserMenu: FC = () => {
-  const router = useRouter();
-  const signOut = () => {
-    router.push(getPath("SIGN_IN"));
-  };
+// const UserMenu: FC = () => {
+//   const router = useRouter();
+//   const signOut = () => {
+//     router.push(getPath("SIGN_IN"));
+//   };
 
-  return (
-    <Menu
-      size="lg"
-      position="bottom"
-      placement="end"
-      transition="pop-top-right"
-      control={
-        <ActionIcon variant="hover" radius="xl" size={40}>
-          <Avatar
-            src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-            radius="xl"
-          />
-        </ActionIcon>
-      }
-      styles={(theme) => {
-        return {
-          label: { fontSize: theme.fontSizes.sm },
-          itemLabel: { fontSize: theme.fontSizes.md },
-        };
-      }}
-    >
-      <Menu.Label>Application</Menu.Label>
-      <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
-        メニュー1
-      </Menu.Item>
-      <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
-        メニュー2
-      </Menu.Item>
-      <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
-        メニュー3
-      </Menu.Item>
-      <Divider />
-      <Menu.Item icon={<Logout size={16} />} onClick={signOut}>
-        ログアウト
-      </Menu.Item>
-    </Menu>
-  );
-};
+//   return (
+// <Menu
+//   size="lg"
+//   position="bottom"
+//   placement="end"
+//   transition="pop-top-right"
+//   control={
+//     <ActionIcon variant="hover" radius="xl" size={40}>
+//       <Avatar
+//         src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+//         radius="xl"
+//       />
+//     </ActionIcon>
+//   }
+//   styles={(theme) => {
+//     return {
+//       label: { fontSize: theme.fontSizes.sm },
+//       itemLabel: { fontSize: theme.fontSizes.md },
+//     };
+//   }}
+// >
+//   <Menu.Label>Application</Menu.Label>
+//   <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
+//     メニュー1
+//   </Menu.Item>
+//   <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
+//     メニュー2
+//   </Menu.Item>
+//   <Menu.Item icon={<Settings size={16} />} component={NextLink} href="#">
+//     メニュー3
+//   </Menu.Item>
+//   <Divider />
+//   <Menu.Item icon={<Logout size={16} />} onClick={signOut}>
+//     ログアウト
+//   </Menu.Item>
+// </Menu>
+//   );
+// };
